dc10841b6f468a99764769920490de6fb312dbd0
Halit <iskitoglu.halitberk@gmail.com>
2026-04-22 01:14:44 +0300

    refactor(admin): split admin UI into ViewComponents

---
+++ DatabaseMastery.TransportMongoDb/ViewComponents/AdminComponents/AdminLayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseMastery.TransportMongoDb.ViewComponents.AdminComponents
+{
+    public class AdminLayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
